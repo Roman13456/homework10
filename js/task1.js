e89d165f46d07ca80cf0819dc5986a49af0cf23b
Roman13456 <romankhromishin@gmail.com>
2022-06-18 16:57:15 +0300

    add reworked slider

--- js/task1.js
+++ js/task1.js
@@ -4,14 +4,20 @@
 // При натисканні на Next - показуємо наступне зображення
 // При натисканні на Prev - попереднє
 // При досягненні останнього зображення – ховати кнопку Next. Аналогічно з першим зображенням та кнопкою
-let imgArray = document.querySelectorAll(".slider img")
+window.onload = function(){
+    let imgArray = document.querySelectorAll(".slider img")
 console.log(imgArray)
 const prevBtn = document.querySelector(".prevBtn")
 const nextBtn = document.querySelector(".nextBtn")
 let imgArrayLength = imgArray.length
 const imgContainer = document.querySelector(".imgContainer")
 let counter = 0
-console.log(nextBtn)
+const slider = document.querySelector(".slider")
+function sliderWidth(){
+    let ImgWidth = imgArray[counter].width
+    slider.style=`width:${ImgWidth}px`
+}
+sliderWidth()
 function btnLimits(){
     if(counter==imgArrayLength-1){
         nextBtn.setAttribute("disabled","")
@@ -24,6 +30,7 @@ function btnLimits(){
         prevBtn.removeAttribute("disabled")
     }
 }
+let imgContainerStyle = ""
 function addTransform(){
     imgContainerStyle+= `transform:translate(${shift}px);`
     imgContainer.style=imgContainerStyle
@@ -40,21 +47,33 @@ function addTransform(){
 //     imgArray[counter].classList.add("visible")
 //     btnLimits()
 // })
-const calcImgContainerWidth = 300*imgArrayLength
+// let ImgContainerWidth = 0
+// function calcImgContainerWidth(){
+//     imgArray.forEach(function(element){
+//         ImgContainerWidth+= element.width
+//     })
+//     console.log(ImgContainerWidth)
+//     let imgContainerStyle = `width:${ImgContainerWidth}px;`
+//     imgContainer.style = imgContainerStyle
+// }
+// calcImgContainerWidth()
+// const calcImgContainerWidth = 300*imgArrayLength
 let shift = 0
-let imgContainerStyle = `width:${calcImgContainerWidth}px;`
-imgContainer.style = imgContainerStyle
 nextBtn.addEventListener("click",function(){
-    shift-=300;
+    shift-=imgArray[counter].width
     console.log(shift)
     addTransform()
     counter+=1
     btnLimits()
+    sliderWidth()
 })
 prevBtn.addEventListener("click",function(){
-    shift+=300;
+    shift+=imgArray[counter-1].width
     console.log(shift)
     addTransform()
     counter-=1
     btnLimits()
+    sliderWidth()
 })
+
+}
